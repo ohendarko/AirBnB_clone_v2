12f16532ba016ccfb50f7ae3f3c33dcfc0fdfd8c
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-15 16:06:01 +0000

    setup_mysql dev & setup_mysql test

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Creates a Databse to check if console cmd Create works.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- FLUSH applies all the privilege changes above without needing to restart MySQL server
+FLUSH PRIVILEGES;
