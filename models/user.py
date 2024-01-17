ad0696c4e410896ae0a803f0098feb3be52504da
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-17 14:57:10 +0000

    done task 7

--- models/user.py
+++ models/user.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """Defines a user by various attributes for mySQL db
+    Inherits from SQLAlchemy's Base and links to a table called user.
+    Attributes:
+    __tablename__ (str): The name of the MySQL table to store users.
+    email: (sqlalchemy String): User email address.
+    password (sqlalchemy String): User password.
+    first_name (sqlalchemy String): User first name.
+    last_name (sqlalchemy String): User last name.
+    """
     __table__name = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
