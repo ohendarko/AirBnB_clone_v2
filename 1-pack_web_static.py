1b7bfeaa2cda9decbd416e8b0723bc5b15388f12
ohendarko <ohendarko@gmail.com>
2024-02-04 18:18:21 +0000

    submit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from fabric import Connection, task,
+from fabric.api import local
+from datetime import datetime
+"""Fabric script that generates a .tgz archive"""
+
+
+@task
+def do_pack(c):
+    """generates a .tgz archive"""
+    local("mkdir -p versions")
+    src = "web_static/"
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    out = f"versions/web_static_{time}.tgz"
+
+    c.local(f"tar -czvf {out} {src}")
+    return out
