91643d75e9145934ec8ddcafd999f4651b794472
ohendarko <ohendarko@gmail.com>
2024-02-04 22:19:53 +0000

    submit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive"""
+"""Fabric script that generates a .tgz archive
+from the contents of the web_static folder."""
 from fabric.api import local
 from datetime import datetime
 
 
-@task
 def do_pack():
     """
     Function that Generates a .tgz archive
