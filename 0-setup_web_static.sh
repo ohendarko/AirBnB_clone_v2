ebf6073be0a81fc80e7b3039b5145113cff3a7f8
ohendarko <ohendarko@gmail.com>
2024-02-04 16:34:30 +0000

    submit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo echo "<html><body>Fake File</body></html>" > /data/web_static/releases/test/index.html
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html><body>Fake File</body></html>" > /data/web_static/releases/test/index.html
 
 #Symbolic links
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
 #Ownership
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
 #Nginx configs
 echo "
@@ -28,6 +28,6 @@ server {
     index index.html;
   }
 }
-" | sudo tee /etc/nginx/sites-available/default
+" | tee /etc/nginx/sites-available/default
 
 sudo service nginx restart
