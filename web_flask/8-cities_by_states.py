d51f118f4dcef5245ca316965d935b6b7d38aada
ohendarko <ohendarko@gmail.com>
2024-02-18 19:49:07 +0000

    submit

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_session(exc):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """display a HTML page"""
+    states = sorted(
+        list(storage.all(State).values()), key=lambda s: s.name
+    )
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
