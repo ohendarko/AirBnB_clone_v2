6933cadc788462f9e2ffb275c9acceb6bcd42e61
ohendarko <ohendarko@gmail.com>
2024-02-18 13:43:59 +0000

    submit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,7 +30,7 @@ def python_text(text='is cool'):
     """display “Python ” followed by the value of the text variable
     (replace underscore _ symbols with a space )"""
     text = text.replace('_', ' ')
-    return f"C {escape(text)}"
+    return f"Python {escape(text)}"
 
 
 if __name__ == "__main__":
