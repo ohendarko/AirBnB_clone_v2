3a74722915d358fc7699888b5746d0cee2aa8a34
ohendarko <ohendarko@gmail.com>
2024-02-18 17:17:56 +0000

    submit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python3
-"""This scripts starts a Flask web application"""
-
-
+"""a script that starts a Flask web application"""
 from flask import Flask
+from markupsafe import escape
 from flask import render_template
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Route that displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+    """/: display “Hello HBNB!" """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Route that displays 'HBNB'"""
-    return 'HBNB'
+    """/hbnb: display “HBNB!" """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Route that displays 'C' followed by value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )"""
+    text = text.replace('_', ' ')
+    return f"C {escape(text)}"
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text='is cool'):
-    """Route that displays 'Python ', followed by value of text variable"""
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """display “Python ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )"""
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """Route that displays 'n is a number' only if n is an integer"""
-    return '{} is a number'.format(n)
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """/number/<n>: display “n is a number”
+    only if n is an integer"""
+    return f"{escape(n)} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template_route(n):
-    "Route that displays an HTML page only if n is an integer"""
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """/number_template/<n>: display an HTML page
+    only if n is an integer:"""
     return render_template('5-number.html', n=n)
 
 
