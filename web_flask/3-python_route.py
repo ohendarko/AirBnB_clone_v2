cbd30bef05b635d2e2762f3c376a0f074e738079
ohendarko <ohendarko@gmail.com>
2024-02-18 13:45:58 +0000

    submit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c_text(text):
     return f"C {escape(text)}"
 
 
+@app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text='is cool'):
     """display “Python ” followed by the value of the text variable
